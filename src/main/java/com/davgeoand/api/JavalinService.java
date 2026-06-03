a58812b17e1bab434abea233450509dbfb4a23d0
David G Andalcio <davega9@gmail.com>
2026-06-03 17:01:04 -0500

    Version Update (June 2026) - unified log message for controller endpoint methods - updated to use influxdb to store events - serviceinfo event added with details of the service when started - updated versions - remove vs code and postman config - removed surrealdb - reformat template

--- src/main/java/com/davgeoand/api/JavalinService.java
+++ src/main/java/com/davgeoand/api/JavalinService.java
@@ -1,7 +1,8 @@
 package com.davgeoand.api;
 
 import com.davgeoand.api.controller.AdminController;
-import com.davgeoand.api.exception.JavalinServiceException.MissingPropertyException;
+import com.davgeoand.api.monitor.event.ServiceEventHandler;
+import com.davgeoand.api.monitor.metric.ServiceMeterRegistry;
 import io.javalin.Javalin;
 import io.javalin.apibuilder.EndpointGroup;
 import io.javalin.event.LifecycleEventListener;
@@ -14,53 +15,49 @@ import static io.javalin.apibuilder.ApiBuilder.path;
 @Slf4j
 public class JavalinService {
     private final Javalin javalin;
-    private final String SERVICE_NAME = ServiceProperties.getProperty("service.name").orElseThrow(() -> new MissingPropertyException("service.name"));
-    private final String SERVICE_CONTEXT_PATH = ServiceProperties.getProperty("service.context.path").orElseThrow(() -> new MissingPropertyException("service.context.path"));
-    private final String SERVICE_PORT = ServiceProperties.getProperty("service.port").orElseThrow(() -> new MissingPropertyException("service.port"));
+    private final String SERVICE_NAME = ServiceProperties.getProperty("service.name");
 
     public JavalinService() {
         log.info("Initializing {}", SERVICE_NAME);
         javalin = Javalin.create(javalinConfig -> {
             javalinConfig.routes.apiBuilder(routes());
-            javalinConfig.router.contextPath = SERVICE_CONTEXT_PATH;
-            javalinConfig.registerPlugin(micrometerRegistry());
+            javalinConfig.router.contextPath = ServiceProperties.getProperty("context.path");
+            javalinConfig.registerPlugin(serviceMeterRegistry());
             javalinConfig.events.serverStarted(serverStartedEvents());
+            javalinConfig.startup.showJavalinBanner = false;
+            javalinConfig.startup.showOldJavalinVersionWarning = false;
         });
         exceptionHandlers();
-        log.info("Initialized {}", SERVICE_NAME);
-    }
-
-    @WithSpan
-    private LifecycleEventListener serverStartedEvents() {
-        log.info("Adding server started events");
-        return () -> {
-            AdminController.addServiceInfo();
-        };
-    }
-
-    @WithSpan
-    private MicrometerPlugin micrometerRegistry() {
-        log.info("Adding micrometer registry");
-        return new MicrometerPlugin(micrometerPluginConfig -> micrometerPluginConfig.registry = ServiceMeterRegistry.meterRegistry);
+        ServiceEventHandler.init();
+        log.info("Finished initializing {}", SERVICE_NAME);
     }
 
     @WithSpan
     private void exceptionHandlers() {
         log.info("Adding exception handlers");
-        log.info("Added exception handlers");
+    }
+
+    @WithSpan
+    private LifecycleEventListener serverStartedEvents() {
+        log.info("Adding server started events");
+        return AdminController::addServiceInfo;
+    }
+
+    @WithSpan
+    private MicrometerPlugin serviceMeterRegistry() {
+        log.info("Adding service meter registry");
+        return new MicrometerPlugin(micrometerPluginConfig -> micrometerPluginConfig.registry = ServiceMeterRegistry.getMeterRegistry());
     }
 
     @WithSpan
     private EndpointGroup routes() {
         log.info("Adding routes");
-        return () -> {
-            path("admin", AdminController.getAdminEndpoints());
-        };
+        return () -> path("admin", AdminController.getAdminEndpoints());
     }
 
     public void start() {
         log.info("Starting {}", SERVICE_NAME);
-        javalin.start(Integer.parseInt(SERVICE_PORT));
+        javalin.start(Integer.parseInt(ServiceProperties.getProperty("port")));
         log.info("Started {}", SERVICE_NAME);
     }
 }
